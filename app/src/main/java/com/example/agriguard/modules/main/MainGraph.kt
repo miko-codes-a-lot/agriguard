fe6ca7c203c89f9ba14d7f4df78629b351dcf367
Reymond Peña <reymond.pena.dev@gmail.com>
2024-12-03 18:05:32 +0800

    add some parameter in the function that i connect in the database, and implement the guard in main graph

--- app/src/main/java/com/example/agriguard/modules/main/MainGraph.kt
+++ app/src/main/java/com/example/agriguard/modules/main/MainGraph.kt
@@ -1,14 +1,17 @@
 package com.example.agriguard.modules.main
 
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import androidx.navigation.toRoute
 import com.example.agriguard.modules.main.dashboard.ui.DashboardUI
-import com.example.agriguard.modules.main.farmer.AddressUI
+import com.example.agriguard.modules.main.farmer.AddressesUI
 import com.example.agriguard.modules.main.farmer.ComplaintFormUI
-import com.example.agriguard.modules.main.farmer.FarmersListUI
+import com.example.agriguard.modules.main.farmer.FarmersUI
 import com.example.agriguard.modules.main.farmer.FarmersPreviewUI
+import com.example.agriguard.modules.main.farmer.viewmodel.FarmersViewModel
 import com.example.agriguard.modules.main.menu.ui.MenuUI
 import com.example.agriguard.modules.main.message.MessageListUI
 import com.example.agriguard.modules.main.message.MessageUI
@@ -25,86 +28,172 @@ import com.example.agriguard.modules.main.report.ui.ReportFormValidationUI
 import com.example.agriguard.modules.main.report.ui.ReportListUI
 import com.example.agriguard.modules.main.report.ui.RiceInsuranceFormUI
 import com.example.agriguard.modules.main.setting.SettingsUI
-import com.example.agriguard.modules.main.user.model.dto.UserDto
+import com.example.agriguard.modules.main.user.model.dto.AddressDto
+import com.example.agriguard.modules.main.user.service.UserService
 import com.example.agriguard.modules.main.user.ui.UserCreateUI
-import com.example.agriguard.modules.main.user.ui.UsersList
+import com.example.agriguard.modules.main.user.ui.UserEditUI
+import com.example.agriguard.modules.main.user.ui.UsersUI
+import com.example.agriguard.modules.main.user.viewmodel.UserViewModel
 import com.example.agriguard.modules.shared.Guard
+import com.example.agriguard.modules.shared.ext.toObjectId
 
 fun NavGraphBuilder.mainGraph(navController: NavController) {
     navigation<MainNav>(startDestination = MainNav.Menu) {
         composable<MainNav.Menu> {
             Guard(navController = navController) { currentUser ->
                 MenuUI(navController, currentUser)
-//                MenuUI(navController, UserDto(isAdmin = true))
             }
         }
         composable<MainNav.Dashboard> {
-            DashboardUI(navController)
-        }
-        composable<MainNav.ComplainForm> {
-            ComplaintFormUI(navController)
-        }
-        composable<MainNav.UsersList> {
-            UsersList(navController)
-        }
-        composable<MainNav.Address> {
-            AddressUI(navController)
-        }
-        composable<MainNav.CreateUser> {
             Guard(navController = navController) { currentUser ->
-                UserCreateUI(navController, currentUser)
+                DashboardUI(navController, currentUser)
             }
         }
-        composable<MainNav.FarmersList> {
-            FarmersListUI(navController)
+        composable<MainNav.ComplainForm> {
+            Guard(navController = navController) { currentUser ->
+                ComplaintFormUI(navController)
+            }
+        }
+        composable<MainNav.Users> {
+            Guard(navController = navController) { currentUser ->
+                UsersUI(
+                    navController,
+                )
+            }
+        }
+        composable<MainNav.EditUser> {
+            val args = it.toRoute<MainNav.EditUser>()
+            val userViewModel: UserViewModel = hiltViewModel()
+            val userDto = userViewModel.fetchUser(args.userId)
+            val userService: UserService = hiltViewModel<UserViewModel>().userService
+            Guard(navController = navController) { currentUser ->
+                UserEditUI(
+                    navController = navController,
+                    currentUser = currentUser,
+                    userDto = userDto,
+                    userService = userService
+                )
+            }
+        }
+        composable<MainNav.Addresses> {
+            Guard(navController = navController) { currentUser ->
+                AddressesUI(navController)
+            }
+        }
+        composable<MainNav.CreateUser> {
+            val args = it.toRoute<MainNav.CreateUser>()
+            val farmersViewModel: FarmersViewModel = hiltViewModel()
+            val addressDto =
+                if(args.addressId != null)
+                    farmersViewModel.fetchOneAddress(args.addressId.toObjectId())
+                else
+                    null
+            val userService: UserService = hiltViewModel<UserViewModel>().userService
+            Guard(navController = navController) { currentUser ->
+                UserCreateUI(
+                    navController = navController,
+                    currentUser = currentUser,
+                    addressDto = addressDto,
+                    userService = userService
+                )
+            }
+        }
+        composable<MainNav.Farmers> {
+            val args = it.toRoute<MainNav.Farmers>()
+            val farmersViewModel: FarmersViewModel = hiltViewModel();
+            var addressDto: AddressDto? = null;
+            if (args.addressId != null) {
+                 addressDto =  farmersViewModel.fetchOneAddress(args.addressId.toObjectId())
+            }
+            Guard(navController = navController) { currentUser ->
+                FarmersUI(
+                    navController = navController,
+                    currentId = currentUser,
+                    addressDto = addressDto,
+                    status = args.status
+                )
+            }
         }
         composable<MainNav.FarmersPreview> {
-            FarmersPreviewUI(navController)
+            Guard(navController = navController) { currentUser ->
+                FarmersPreviewUI(navController, currentUser)
+            }
         }
         composable<MainNav.Message> {
-            MessageUI(navController)
+            Guard(navController = navController) { currentUser ->
+                MessageUI(navController)
+            }
         }
         composable<MainNav.MessageList> {
-            MessageListUI(navController)
+            Guard(navController = navController) { currentUser ->
+                MessageListUI(navController)
+            }
         }
         composable<MainNav.NotificationList> {
-            NotificationListUI(navController)
+            Guard(navController = navController) { currentUser ->
+                NotificationListUI(navController)
+            }
         }
         composable<MainNav.FormValidation> {
-            ReportFormValidationUI(navController)
+            Guard(navController = navController) { currentUser ->
+                ReportFormValidationUI(navController)
+            }
         }
         composable<MainNav.Setting> {
-            SettingsUI(navController)
+            Guard(navController = navController) { currentUser ->
+                val userService: UserService = hiltViewModel<UserViewModel>().userService
+                SettingsUI(navController, currentUser = currentUser, userService = userService)
+            }
         }
         composable<MainNav.RiceInsurance> {
-            RiceInsuranceFormUI()
+            Guard(navController = navController) { currentUser ->
+                RiceInsuranceFormUI()
+            }
         }
         composable<MainNav.InDemnity> {
-            InDemnityFormUI()
+            Guard(navController = navController) { currentUser ->
+                InDemnityFormUI()
+            }
         }
         composable<MainNav.OnionInsurance> {
-            OnionInsuranceFormUI()
+            Guard(navController = navController) { currentUser ->
+                OnionInsuranceFormUI()
+            }
         }
         composable<MainNav.RiceDisease> {
-            RiceDiseaseUI(navController)
+            Guard(navController = navController) { currentUser ->
+                RiceDiseaseUI(navController)
+            }
         }
         composable<MainNav.OnionDisease> {
-            OnionDiseaseUI()
+            Guard(navController = navController) { currentUser ->
+                OnionDiseaseUI()
+            }
         }
         composable<MainNav.RicePets> {
-            RicePetsModule()
+            Guard(navController = navController) { currentUser ->
+                RicePetsModule()
+            }
         }
         composable<MainNav.OnionPets> {
-            OnionPetsModule()
+            Guard(navController = navController) { currentUser ->
+                OnionPetsModule()
+            }
         }
         composable<MainNav.OnionWeed> {
-            OnionWeedUI()
+            Guard(navController = navController) { currentUser ->
+                OnionWeedUI()
+            }
         }
         composable<MainNav.RiceWeed> {
-            RiceWeedUI()
+            Guard(navController = navController) { currentUser ->
+                RiceWeedUI()
+            }
         }
         composable<MainNav.ReportList> {
-            ReportListUI(navController)
+            Guard(navController = navController) { currentUser ->
+                ReportListUI(navController)
+            }
         }
     }
 }
