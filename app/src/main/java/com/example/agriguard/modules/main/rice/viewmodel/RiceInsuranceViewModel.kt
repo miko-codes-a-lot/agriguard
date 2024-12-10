ddfef22bba011f7c3bbe28b4d250818757144143
Miko Chu <miko.codecrafters@gmail.com>
2024-12-10 17:21:21 +0800

    add rice insurance view model to handle form state and upsert

---
+++ app/src/main/java/com/example/agriguard/modules/main/rice/viewmodel/RiceInsuranceViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.agriguard.modules.main.rice.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.agriguard.modules.main.rice.service.RiceInsuranceService
+import com.example.agriguard.modules.main.rice.model.dto.RiceInsuranceDto
+import com.example.agriguard.modules.main.user.model.dto.UserDto
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class RiceInsuranceViewModel @Inject constructor(
+    val service: RiceInsuranceService,
+) : ViewModel() {
+    private val _formState = MutableStateFlow(RiceInsuranceDto())
+    val formState: StateFlow<RiceInsuranceDto> = _formState
+
+    fun updateField(field: (RiceInsuranceDto) -> RiceInsuranceDto) {
+        _formState.value = field(_formState.value)
+    }
+
+    suspend fun upsert(data: RiceInsuranceDto, currentUser: UserDto): Result<RiceInsuranceDto> {
+        return service.upsert(data, currentUser)
+    }
+}
