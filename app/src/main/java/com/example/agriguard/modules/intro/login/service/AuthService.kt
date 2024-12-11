af95094dc43f375365c34bafc39a2b49dfcdfda3
Miko Chu <miko.codecrafters@gmail.com>
2024-12-11 23:24:30 +0800

    auth service add specific data subscription by logged in user

--- app/src/main/java/com/example/agriguard/modules/intro/login/service/AuthService.kt
+++ app/src/main/java/com/example/agriguard/modules/intro/login/service/AuthService.kt
@@ -1,17 +1,26 @@
 package com.example.agriguard.modules.intro.login.service
 
 import com.example.agriguard.modules.intro.login.model.dto.LoginDto
+import com.example.agriguard.modules.main.notify.model.entity.Notify
 import com.example.agriguard.modules.main.user.model.dto.UserDto
 import com.example.agriguard.modules.main.user.model.entity.User
 import com.example.agriguard.modules.main.user.model.mapper.toDTO
 import com.example.agriguard.modules.shared.ext.verifyPassword
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
+import io.realm.kotlin.mongodb.subscriptions
+import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
 class AuthService @Inject constructor(
     private val realm: Realm,
 ) {
+    suspend fun loadSpecificDataByUser(currentUserId: String) {
+        realm.subscriptions.update {
+            remove(Notify.COLLECTION_NAME)
+            add(realm.query<Notify>("receiver == $0", ObjectId(currentUserId)), Notify.COLLECTION_NAME)
+        }
+    }
 
     fun login(loginDto: LoginDto): UserDto? {
         val query = "email == $0"
