65867371db127c734bf4b6f3312e78505c4edf04
Miko Chu <miko.codecrafters@gmail.com>
2024-12-11 23:28:54 +0800

    viewmodel add load specific data sync and improve logout

--- app/src/main/java/com/example/agriguard/modules/intro/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/agriguard/modules/intro/login/viewmodel/LoginViewModel.kt
@@ -3,7 +3,6 @@ package com.example.agriguard.modules.intro.login.viewmodel
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.example.agriguard.modules.intro.IntroNav
@@ -11,10 +10,7 @@ import com.example.agriguard.modules.intro.login.model.dto.LoginDto
 import com.example.agriguard.modules.intro.login.service.AuthService
 import com.example.agriguard.modules.main.user.model.dto.UserDto
 import com.example.agriguard.modules.main.user.service.UserService
-import com.example.agriguard.modules.shared.ext.hashPassword
-import com.example.agriguard.modules.shared.ext.verifyPassword
 import dagger.hilt.android.lifecycle.HiltViewModel
-import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
 @HiltViewModel
@@ -36,6 +32,11 @@ class LoginViewModel @Inject constructor(
             return true
         }
         return false
+    }
+
+    suspend fun loadSpecificDataByUser() {
+        val userId = getLoggedInUserId()!!
+        authService.loadSpecificDataByUser(userId)
     }
 
     private fun saveUserSession(userId: String) {
@@ -63,12 +64,10 @@ class LoginViewModel @Inject constructor(
     }
 
     fun logout(navController: NavController) {
-        val userId = getLoggedInUserId()
-        if (userId != null) {
-            clearUserSession()
-            navController.navigate(IntroNav) {
-                popUpTo(0) { inclusive = true }
-            }
+        clearUserSession()
+        navController.navigate(IntroNav) {
+            popUpTo<IntroNav.Login> { inclusive = true }
+            launchSingleTop = true
         }
     }
 }
