2c198df7caeb7e9636670588cc428ead87b6df58
Reymond Pena <reymond.pena.dev@gmail.com>
2024-12-12 22:31:07 +0800

    add design check box field

--- app/src/main/java/com/example/agriguard/modules/shared/ui/CheckBoxField.kt
+++ app/src/main/java/com/example/agriguard/modules/shared/ui/CheckBoxField.kt
@@ -1,18 +1,24 @@
 package com.example.agriguard.modules.shared.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CheckBoxField(label: String, value: Boolean, onChange: (Boolean) -> Unit) {
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(modifier = Modifier.
+        width(150.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ){
         Checkbox(checked = value, onCheckedChange = onChange)
-        Text(label, modifier = Modifier.padding(start = 8.dp))
+        Text(label, modifier = Modifier.padding(start = 1.dp))
     }
 }
