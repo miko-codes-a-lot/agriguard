407da0c2a5386cf4b97fa3679d824b8cd161f716
Reymond Pena <reymond.pena.dev@gmail.com>
2024-12-12 23:21:37 +0800

    change the location import of indemnity in indemnity view model

--- app/src/main/java/com/example/agriguard/modules/main/indemnity/viewmodel/IndemnityViewModel.kt
+++ app/src/main/java/com/example/agriguard/modules/main/indemnity/viewmodel/IndemnityViewModel.kt
@@ -2,7 +2,7 @@ package com.example.agriguard.modules.main.indemnity.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.agriguard.modules.main.indemnity.service.IndemnityInsuranceService
-import com.example.agriguard.modules.main.user.model.dto.IndemnityDto
+import com.example.agriguard.modules.main.indemnity.model.dto.IndemnityDto
 import com.example.agriguard.modules.main.user.model.dto.UserDto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
