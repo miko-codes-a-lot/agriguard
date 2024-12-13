3ffc20f1ee095da2e4e1512be4045e1b9058845b
Reymond Pena <reymond.pena.dev@gmail.com>
2024-12-13 15:04:18 +0800

    add condition statement for fetch all function in onion service nad view model

--- app/src/main/java/com/example/agriguard/modules/main/onion/service/OnionInsuranceService.kt
+++ app/src/main/java/com/example/agriguard/modules/main/onion/service/OnionInsuranceService.kt
@@ -3,27 +3,30 @@ package com.example.agriguard.modules.main.onion.service
 import com.example.agriguard.modules.main.onion.mapper.toDTO
 import com.example.agriguard.modules.main.onion.mapper.toEntity
 import com.example.agriguard.modules.main.onion.model.dto.OnionInsuranceDto
+import com.example.agriguard.modules.main.onion.model.dto.OnionWithUserDto
 import com.example.agriguard.modules.main.onion.model.entity.OnionInsurance
-import com.example.agriguard.modules.main.rice.mapper.toDTO
-import com.example.agriguard.modules.main.rice.mapper.toEntity
-import com.example.agriguard.modules.main.rice.model.dto.RiceInsuranceDto
-import com.example.agriguard.modules.main.rice.model.entity.RiceInsurance
 import com.example.agriguard.modules.main.user.model.dto.UserDto
+import com.example.agriguard.modules.main.user.service.UserService
 import com.example.agriguard.modules.shared.ext.toInstantString
 import io.realm.kotlin.Realm
 import io.realm.kotlin.UpdatePolicy
 import io.realm.kotlin.ext.query
 import io.realm.kotlin.query.Sort
 import io.realm.kotlin.types.RealmInstant
+import org.mongodb.kbson.ObjectId
 import javax.inject.Inject
 
-class OnionInsuranceService @Inject constructor(private val realm: Realm)  {
+class OnionInsuranceService @Inject constructor(
+    private val userService: UserService,
+    private val realm: Realm,
+)  {
     suspend fun upsert(data: OnionInsuranceDto, currentUser: UserDto): Result<OnionInsuranceDto> {
         val dateNow = RealmInstant.now().toInstantString()
         if (data.id == null) {
             data.userId = currentUser.id!!
             data.createdById = currentUser.id!!
             data.createdAt = dateNow
+            data.reviewById = currentUser.createdById
         }
         data.lastUpdatedById = currentUser.id!!
         data.lastUpdatedAt = dateNow
@@ -38,10 +41,40 @@ class OnionInsuranceService @Inject constructor(private val realm: Realm)  {
         }
     }
 
-    fun fetchListInsurance(userId: String): List<OnionInsuranceDto> {
-        return realm.query<OnionInsurance>("userId == $0", userId)
+    fun fetchAll(userDto: UserDto): List<OnionWithUserDto> {
+        val queryBuilder = StringBuilder()
+
+        if (userDto.isFarmers) queryBuilder.append("userId == $0")
+        if (userDto.isTechnician) queryBuilder.append("reviewById == $0")
+
+        val userId = if (userDto.isAdmin) null
+        else if (userDto.isTechnician) ObjectId(userDto.id!!)
+        else userDto.id!!
+
+        val query = if (userDto.isAdmin) realm.query<OnionInsurance>()
+        else realm.query<OnionInsurance>(queryBuilder.toString(), userId)
+
+        val userIndex = mutableMapOf<String, UserDto>()
+
+        val entries = query
             .sort("fillUpDate", Sort.DESCENDING)
             .find()
             .map { it.toDTO() }
+
+        return entries.map {
+            val user = userIndex[it.userId] ?: userService.fetchOne(it.userId)
+            userIndex[it.userId] = user
+
+            OnionWithUserDto(it, user)
+        }
+    }
+
+    fun fetchOne(id: String): OnionInsuranceDto {
+        return realm.query<OnionInsurance>("_id == $0", ObjectId(id))
+            .find()
+            .first()
+            .run {
+                toDTO()
+            }
     }
 }
--- app/src/main/java/com/example/agriguard/modules/main/onion/viewmodel/OnionInsuranceViewmodel.kt
+++ app/src/main/java/com/example/agriguard/modules/main/onion/viewmodel/OnionInsuranceViewmodel.kt
@@ -2,8 +2,8 @@ package com.example.agriguard.modules.main.onion.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.agriguard.modules.main.onion.model.dto.OnionInsuranceDto
+import com.example.agriguard.modules.main.onion.model.dto.OnionWithUserDto
 import com.example.agriguard.modules.main.onion.service.OnionInsuranceService
-import com.example.agriguard.modules.main.rice.model.dto.RiceInsuranceDto
 import com.example.agriguard.modules.main.user.model.dto.UserDto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,15 +17,23 @@ class OnionInsuranceViewmodel  @Inject constructor(
     private val _formState = MutableStateFlow(OnionInsuranceDto())
     val formState: StateFlow<OnionInsuranceDto> = _formState
 
+    fun setFormState(onionDto: OnionInsuranceDto) {
+        _formState.value = onionDto
+    }
+
     fun updateField(field: (OnionInsuranceDto) -> OnionInsuranceDto) {
         _formState.value = field(_formState.value)
+    }
+
+    fun fetchOne(id: String): OnionInsuranceDto {
+        return service.fetchOne(id)
     }
 
     suspend fun upsert(data: OnionInsuranceDto, currentUser: UserDto): Result<OnionInsuranceDto> {
         return service.upsert(data, currentUser)
     }
 
-    fun fetchListOnionInsurance(userId: String): List<OnionInsuranceDto> {
-        return service.fetchListInsurance(userId)
+    fun fetchAll(userDto: UserDto): List<OnionWithUserDto> {
+        return service.fetchAll(userDto)
     }
 }
